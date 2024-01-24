0b041f186d51231c9e602e4b56b446666a00e2b0
YoungHee Han <hyhline@gmail.com>
2024-01-24 04:57:29 +0000

    WIP

---
+++ qpizza-api/src/main/java/qpizza/GreetingResource.java
@@ -0,0 +1,16 @@
+package qpizza;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "안녕하세요.. 피자 드실 건가요?";
+    }
+}
